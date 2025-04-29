020cfa44c6d40e8948e8cca0873268c642ea5bfb
OlenkaTumak <olenatumak52@gmail.com>
2025-04-29 20:30:49 +0300

    Binary Tree Traversal solution

---
+++ Binary_Tree_Traversal/Binary_Tree_Traversal.py
@@ -0,0 +1,17 @@
+# Pre-order traversal
+def pre_order(node):
+    if node:
+        return [node.data] + pre_order(node.left) + pre_order(node.right)
+    return []
+
+# In-order traversal
+def in_order(node):
+    if node:
+        return in_order(node.left) + [node.data] + in_order(node.right)
+    return []
+
+# Post-order traversal
+def post_order(node):
+    if node:
+        return post_order(node.left) + post_order(node.right) + [node.data]
+    return []
